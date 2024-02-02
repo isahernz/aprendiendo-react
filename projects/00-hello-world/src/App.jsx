b191a764ad2c6f36ab2183825a61789129f15ae1
Brandon Isahir Hernández Camacho <isahernz@outlook.com>
2024-02-02 16:01:08 -0600

    feat: :sparkles: add my first components

--- projects/00-hello-world/src/App.jsx
+++ projects/00-hello-world/src/App.jsx
@@ -1,4 +1,38 @@
 import { TwitterFollowCard } from "./TwitterFollowCard";
 import "./App.css";
 
-export const App = () => <TwitterFollowCard />;
+const users = [
+  {
+    name: "Brandon Hernández",
+    username: "isahernz",
+    isFollowing: true,
+  },
+
+  {
+    name: "Laravel",
+    username: "laravel",
+    isFollowing: false,
+  },
+
+  {
+    name: "Astro",
+    username: "withastro",
+    isFollowing: true,
+  },
+];
+
+export const App = () => {
+  return (
+    <section className="App">
+      {users.map(({ name, username, isFollowing }) => (
+        <TwitterFollowCard
+          key={username}
+          username={username}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
+    </section>
+  );
+};
--- projects/00-hello-world/src/TwitterFollowCard.jsx
+++ projects/00-hello-world/src/TwitterFollowCard.jsx
@@ -1,19 +1,35 @@
-export const TwitterFollowCard = () => {
+import { useState } from "react";
+export const TwitterFollowCard = (props) => {
+  const [isFollowing, setIsFollowing] = useState(props.initialIsFollowing);
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+
+  const text = isFollowing ? "Following" : "Follow";
+
+  const buttonClass = isFollowing
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button";
+
   return (
     <article className="tw-followCard">
       <header className="tw-followCard-header">
         <img
           className="tw-followCard-avatar"
-          src="https://unavatar.io/isahernz"
-          alt="El avatar de isahernz"
+          src={`https://unavatar.io/${props.username}`}
+          alt={`El avatar de ${props.name}`}
         />
         <div className="tw-followCard-info">
-          <strong>Isa.jsx</strong>
-          <span className="tw-followCard-infoUserName">@_isahernz</span>
+          <strong>{props.children}</strong>
+          <span className="tw-followCard-infoUserName">@{props.username}</span>
         </div>
       </header>
       <aside>
-        <button className="tw-followCard-button">Seguir</button>
+        <button className={buttonClass} onClick={handleClick}>
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-unFollow">Dejar de seguir</span>
+        </button>
       </aside>
     </article>
   );
